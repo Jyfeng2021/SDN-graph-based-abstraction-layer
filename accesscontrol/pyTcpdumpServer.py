10c1fe998e5c1fe50e825ebdc3cd0225df2092bf
Jyfeng2021 <131986656+Jyfeng2021@users.noreply.github.com>
2025-01-18 12:47:14 +0800

    Update pyTcpdumpServer.py

--- accesscontrol/pyTcpdumpServer.py
+++ accesscontrol/pyTcpdumpServer.py
@@ -5,11 +5,9 @@ from app.dataBase_candle import FindAuth
 from app.flowTable_operate import PostOperation
 
 """
-tcpdump抓包服务端：
-主要用来接收抓包监测的终端访问资源服务器的连接信息，将抓到的包发送到这里进行处理，
 得到源ip、目的ip、目的端口等信息，进而去数据库寻找此连接权限，若权限有，则下发相对应的通过流表项
+Get the source ip address, destination ip address, destination port and other information, and then go to the database to find the connection permission, if the permission is available, the corresponding through the flow entry is issued
 
-本系统的服务端采用多进程实现socket客户端的多连接，可以处理接收多个抓包程序发送过来的信息。
 """
 
 
@@ -19,24 +17,23 @@ class Server1:
         self.port = port
         self.lst_num = lst_num
 
-    # 数据处理
+
     def recv_message(self, conn, addr):
         data = conn.recv(1024)
-        # 对收到的抓包信息进行解析，得出源ip、目的ip、目的端口等信息
+
         result = re.findall(".*IP (.*): Flags.*", data.decode('utf-8'))
         list_res = result[0].split('> ')
         src_ip = '.'.join(list_res[0].split('.')[:4])
         dst_ip, dst_port = '.'.join(list_res[1].split('.')[0:4]), list_res[1].split('.')[4:]
         # print(src_ip, dst_ip, dst_port[0])
 
-        # 获取权限
+        #Get permission
         find_auth = FindAuth()
         auth_res = find_auth.get_data(src_ip=src_ip, dst_ip=dst_ip, dst_port=dst_port[0])
         as_res = find_auth.get_as(src_ip=src_ip)
         print("from {0}:".format(addr), data.decode('utf-8'))
-        print('权限：', auth_res)
+        print('Limits of authority：', auth_res)
 
-        # 根据所属自治域选择向不同的控制器下发流表项
         if as_res == 'as1':
             postTable = PostOperation('172.17.0.2', '8080')
             postTable.post_add_flow(src_ip=src_ip, dst_ip=dst_ip, dst_port=dst_port[0], auth=auth_res)
@@ -44,7 +41,7 @@ class Server1:
             postTable = PostOperation('172.17.0.3', '8080')
             postTable.post_add_flow(src_ip=src_ip, dst_ip=dst_ip, dst_port=dst_port[0], auth=auth_res)
 
-    # 服务端的数据接收，在调用时使用多进程
+
     def server_link(self, conn, addr):
         conn.send("Welcome connect!".encode())
 
@@ -56,11 +53,10 @@ class Server1:
 
         conn.close()
 
-    # 服务端的启动程序
+    # 服务端的启动程序  Server side startup program
     def server_start(self):
         # IPv4
         s_pro = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # 操作系统会在服务器socket被关闭或服务器进程终止后马上释放该服务器的端口
         s_pro.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s_pro.bind((self.host, self.port))
         s_pro.listen(self.lst_num)
@@ -68,7 +64,6 @@ class Server1:
         while True:
             conn, addr = s_pro.accept()
             print("Success connect from ", addr)
-            # 启动多进程实现多连接
             p = Process(target=self.server_link, args=(conn, addr))
             p.start()
 
