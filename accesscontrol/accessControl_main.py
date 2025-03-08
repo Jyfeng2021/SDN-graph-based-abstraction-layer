78c9c423c02f06c692723edb683f8a3bca46cb84
Jyfeng2021 <131986656+Jyfeng2021@users.noreply.github.com>
2025-03-08 14:13:24 +0800

    Update accessControl_main.py

--- accesscontrol/accessControl_main.py
+++ accesscontrol/accessControl_main.py
@@ -1,11 +1,1 @@
-from app.flowTable_operate import PostOperation
-from monitor.pyTcpdumpServer import Server1
 
-if __name__ == '__main__':
-    ## Initialize the flow
-    init = PostOperation('172.17.0.2', '8080')
-    init.init_add_flow('172.17.0.2', '172.17.0.3', '8080', '8080')
-
-    # 开启服务端程序 # Start the server program
-    server = Server1('10.0.2.15', 62121, 2)
-    server.server_start()
